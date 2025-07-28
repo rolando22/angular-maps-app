2c3c2aca83be4ddeeb1da90522451d5791c3ae94
rolando22 <rolando2207@gmail.com>
2025-07-28 17:18:54 -0300

    add: project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen-map',
+    title: 'Mapa Completo',
+    loadComponent: () => import('@pages/fullscreen-map-page/fullscreen-map-page.component'),
+  },
+  {
+    path: 'markers',
+    title: 'Marcadores',
+    loadComponent: () => import('@pages/markers-page/markers-page.component'),
+  },
+  {
+    path: 'houses',
+    title: 'Propiedades disponibles',
+    loadComponent: () => import('@pages/houses-page/houses-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen-map',
+  },
+];
---
+++ src/app/pages/houses-page/houses-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-page',
+  imports: [],
+  templateUrl: './houses-page.component.html',
+})
+export default class HousesPageComponent { }
